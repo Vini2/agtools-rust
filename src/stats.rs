44e349f1eb5b311b8990fa6942980b6fe637d12e
Vijini Mallawaarachchi <viji.mallawaarachchi@gmail.com>
2025-07-08 13:38:47 +0930

    DEV: update main cli

---
+++ src/stats.rs
@@ -0,0 +1,8 @@
+use crate::args::StatsCommand;
+
+pub fn run(cmd: &StatsCommand) {
+    // Here is where you would add your real merge logic!
+    println!("Obtaining statistics from the following GFA file: {:?}", cmd.graph);
+    println!("Output will be placed in: {}", cmd.output);
+    println!("Stats logic would run here!");
+}
